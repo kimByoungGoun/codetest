6335fcea9256b60af4b2e677535a544a7efea099
kgun610@naver.com <kgun610@naver.com>
2020-09-16 17:44:56 +0900

    20200916 baekjoon_1100 하얀 칸

--- 1.문제 해결 패러다임/완전탐색/200810_baekjoon_1100/byounggoun/main.py
+++ 1.문제 해결 패러다임/완전탐색/200810_baekjoon_1100/byounggoun/main.py
@@ -4,12 +4,13 @@ if __name__ == '__main__':
     count = 0
     for i in range(8):
         rl = sys.stdin.readline()
-        if i%2 == 0:
-            if rl[i] == 'F':
-                count = count + 1
-        else:
-            if rl[i] == 'F':
-                count = count + 1
+        for j in range(8):
+            if (i+j)%2 == 0:
+                if rl[j] == 'F':
+                    count = count + 1
+            else:
+                continue
+
         #print(i)
 
             
