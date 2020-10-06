e9d483712fef44ca23d7cf91638f5c7a9b81b69e
kgun610@naver.com <kgun610@naver.com>
2020-10-06 09:40:50 +0900

    20201007 baekjoon 1932 finish 정수 삼각형

--- 1.문제 해결 패러다임/동적 계획/201005_baekjoon_1932/byounggoun/main.py
+++ 1.문제 해결 패러다임/동적 계획/201005_baekjoon_1932/byounggoun/main.py
@@ -1,4 +1,5 @@
 import sys
+import copy
 
 def max(a, b):
     if(a > b):
@@ -7,19 +8,30 @@ def max(a, b):
         return b
 
 if __name__ == "__main__":
+    before_line = list()
     line = list()
     input_list = list()
-    count = input()
+    count = int(input())
     line.append(int(input()))
-    for i in range(1, int(count)):
+
+    if count >= 2:
         input_list = input().split(" ")
+        line.append(int(input_list[1]) + line[0])
+        line[0] = int(input_list[0]) + line[0]
+        
+    before_line = copy.deepcopy(line)
 
-        for j in range(1, i-1):
-            line[j] = max(line[j-1], line[j]) + int(input_list[j])
+    for i in range(2, count):
+        input_list = input().split(" ")
+        
+        for j in range(1, i):
+            line[j] = max(before_line[j-1], before_line[j]) + int(input_list[j])
 
-        line[0] = int(input_list[0]) + int(line[0])
-        line.append(int(input_list[i]) + int(line[i-1]))
-        print(line)
+
+        line[0] = int(input_list[0]) + int(before_line[0])
+        line.append(int(input_list[i]) + int(before_line[i-1]))
+        before_line = copy.deepcopy(line)
+
 
     result = 0
     for i in range(int(count)):
