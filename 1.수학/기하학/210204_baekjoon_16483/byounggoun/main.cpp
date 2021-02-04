f693115afdcb093a89167026fd699cf5ca7d5ac4
unknown <kgun610@naver.com>
2021-02-04 21:21:05 +0900

    20210204 baekjoon_16483 접시 안의 원

---
+++ 1.수학/기하학/210204_baekjoon_16483/byounggoun/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main()
+{
+	double t;
+	int result;
+	cin >> t;
+	result = round(pow(t / 2,2));
+	cout << result << endl;
+	return 0;
+}
