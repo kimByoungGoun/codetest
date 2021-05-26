b009e1a175ae8383368a8fc9cdac3d734cc1ebb9
kgun610 <kgun610@naver.com>
2021-05-26 16:54:48 +0900

    20210526 baekjoon_2559 수열

---
+++ 1.수학/부분합/210526_baekjoon_2559/byounggoun/main.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int nums[100001];
+int main()
+{
+
+    int count, K;
+    cin >> count >> K;
+    nums[0] = 0;
+    for (int i = 1; i < count+1; i++)
+    {
+        cin >> nums[i];
+        nums[i] = nums[i] + nums[i-1];
+    }
+ 
+    int result = -100000;
+    
+    for (int i = 1; i < count - K + 2; i++)
+    {
+        int sum = 0;
+        sum = nums[i + K - 1] - nums[i - 1];
+        if (sum > result)
+        {
+            result = sum;
+        }
+    }
+    cout << result << endl;
+    return 0;
+}
