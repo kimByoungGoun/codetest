d979013b018bc6b288d5e51eac933dc1300b49b9
kgun610 <kgun610@naver.com>
2021-06-01 18:04:47 +0900

    20210601 baekjoon_2217 로프

---
+++ 0.기초/정렬/20210601_baekjoon_2217/byounggoun/main.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int loap[100001];
+int max(int a, int b)
+{
+    if (a > b) { return a; }
+    else { return b; }
+}
+int main()
+{
+    int count, result = 0;
+    cin >> count;
+     
+    for (int i = 0; i < count; i++)
+    {
+        cin >> loap[i];
+    }
+    sort(loap, loap + count);
+    for (int i = 0; i < count; i++)
+    {
+        result = max(result, loap[i] * (count - i));
+    }
+    cout << result << endl;
+    return 0;
+}
