1a5674892a2165ffbf25df1c0eaaea89eee6739a
kgun610 <kgun610@naver.com>
2021-06-16 18:13:40 +0900

    20210616 baekjoon_4673 셀프 넘버

---
+++ 0.기초/20210616_baekjoon_4673/byounggoun/main.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#define N 10001 
+
+using namespace std;
+bool arr[N];
+
+int removeSelfNum(int n) {
+    int sum = n;   
+    while (1) {  
+
+        if (n == 0)
+        {
+            break;
+        }
+        sum += n % 10;      
+        n = n / 10;        
+
+    }
+    return sum;
+}
+
+
+int main(void) {
+
+    for (int i = 1; i < N; i++) {
+        int idx = removeSelfNum(i);
+
+        if (idx <= N) {
+            arr[idx] = true;
+        }
+
+    }
+
+    for (int i = 1; i < N; i++) {
+        if (arr[i] == false)
+        {
+           cout << i << endl;
+        }
+    }
+
+    return 0;
+}
+
