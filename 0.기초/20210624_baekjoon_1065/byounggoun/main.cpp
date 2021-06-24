78836c91ff408bb44dc5bd31ddb16aa909138a28
kgun610 <kgun610@naver.com>
2021-06-24 11:21:04 +0900

    20210624 baekjoon_1065 한수

---
+++ 0.기초/20210624_baekjoon_1065/byounggoun/main.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+using namespace std;
+
+bool check(int n) {  
+    int diff;
+    if (n < 10)
+    {
+        return true;
+    }
+    diff = ((n / 10) % 10) - (n % 10);
+    n = n / 10;
+    while (1) {  
+        if (n < 10)
+        {
+            return true;
+        }
+        if (((n / 10) % 10) - (n % 10) != diff)
+        {
+            return false;
+        }
+        n = n / 10;
+
+    }
+}
+
+
+int main(void) {
+    int result = 0;
+    int N;
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        
+        if (check(i) == true) {
+            result++;
+        }
+
+    }
+    cout << result << endl;
+
+    return 0;
+}
+
