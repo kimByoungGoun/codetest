f5f18ce1fa9b5476ea620290f4e5895ecd56ab5d
kgun610 <kgun610@naver.com>
2021-06-30 15:51:39 +0900

    20210630 baekjoon_2941 크로아티아 알파벳

---
+++ 1.문자열 처리/문자열 매칭/210630_baekjoon_2941/byounggoun/main.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector <string> alphabets;
+string input;
+int init()
+{
+    alphabets.push_back("c=");
+    alphabets.push_back("c-");
+    alphabets.push_back("dz=");
+    alphabets.push_back("d-");
+    alphabets.push_back("lj");
+    alphabets.push_back("nj");
+    alphabets.push_back("s=");
+    alphabets.push_back("z=");
+    return 0;
+}
+
+int main(void) {
+    init();
+
+    cin >> input;
+    int length = input.length();
+    int result = length;
+    for (int i = 0; i < length; i++) {
+        
+        for (int j = 0; j < alphabets.size(); j++)
+        {
+            if (i + alphabets[j].size() <= length)
+            {
+                string target = input.substr(i, alphabets[j].size());
+                if ((alphabets[j].compare(target)) == 0)
+                {
+                    result = result - (alphabets[j].size()-1);
+                    i = i + (alphabets[j].size() - 1);
+                }
+            }
+            else
+            {
+                continue;
+            }
+        }
+
+    }
+    cout << result << endl;
+
+    return 0;
+}
