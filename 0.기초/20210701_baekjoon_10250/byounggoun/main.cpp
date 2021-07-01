85e996e84deda6dc5441e4ad07e95d5573e96724
kgun610 <kgun610@naver.com>
2021-07-01 17:25:07 +0900

    20210701 baekjoon_10250 ACM 호텔

---
+++ 0.기초/20210701_baekjoon_10250/byounggoun/main.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int count;
+    cin >> count;
+    for (int i = 0; i < count; i++)
+    {
+        int H, W, target;
+        cin >> H >> W >> target;
+        int a = target % H;
+        int b = target / H + 1;
+        if (a == 0)
+        {
+            a = H;
+            b = b - 1;
+        }
+        int result = a * 100 + b;
+        cout << result << endl;
+    }
+    
+
+    return 0;
+}
