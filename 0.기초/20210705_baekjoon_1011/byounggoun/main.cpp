314c492e699f14a6d1394c62406ac151e91402d8
kgun610 <kgun610@naver.com>
2021-07-05 17:02:36 +0900

    20210705 baekjoon_1011 Fly me to the Alpha Centauri

---
+++ 0.기초/20210705_baekjoon_1011/byounggoun/main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int count;
+    cin >> count;
+    for (int i = 0; i < count; i++)
+    {
+        int A, B, result = 0, distance;
+        cin >> A >> B;
+        distance = B - A;
+        for (int j = 0;; j++)
+        {
+            if (distance <= 0)
+            {
+                cout << j << endl;
+                break;
+            }
+            distance = distance - ((j+2) / 2);
+
+        }
+
+    }
+    
+
+    return 0;
+}
