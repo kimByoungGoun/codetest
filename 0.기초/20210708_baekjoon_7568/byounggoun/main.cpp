0565111748207f1dae87a1668cd13321dddb00e4
kgun610 <kgun610@naver.com>
2021-07-08 15:49:11 +0900

    20210708 beakjoon_7568 덩치

---
+++ 0.기초/20210708_baekjoon_7568/byounggoun/main.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+    int count;
+    cin >> count;
+    vector <pair<int, int>> people;
+    vector <int> result;
+    for (int i = 0; i < count; i++)
+    {
+        pair<int, int> input;
+        cin >> input.first >> input.second;
+        people.push_back(input);
+    }
+    for (int i = 0; i < count; i++)
+    {
+        int tmp = 0;
+        for (int j = 0; j < count; j++)
+        {
+            if (j == i)
+            {
+                continue;
+            }
+            else
+            {
+                if (people[i].first < people[j].first && people[i].second < people[j].second)
+                {
+                    tmp++;
+                }
+            }
+        }
+        result.push_back(tmp + 1);
+    }
+    for (int i = 0; i < count; i++)
+    {
+        cout << result[i] << endl;
+    }
+
+    return 0;
+}
