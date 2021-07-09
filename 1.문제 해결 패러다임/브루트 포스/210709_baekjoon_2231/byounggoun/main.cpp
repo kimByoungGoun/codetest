a0f3698c85fb48ee4ed3a11232dbf3fb6f392ebb
kgun610 <kgun610@naver.com>
2021-07-09 09:56:03 +0900

    20210709 baekjoon_2231 분해합

---
+++ 1.문제 해결 패러다임/브루트 포스/210709_baekjoon_2231/byounggoun/main.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int num, result;
+    cin >> num;
+    for (int i = 1;; i++)
+    {
+        if (i > num)
+        {
+            result = 0;
+            break;
+        }
+
+        int tmp = i;
+        int tmp_result = i;
+        for (;;)
+        {
+            tmp_result = tmp_result + (tmp % 10);
+            tmp = tmp / 10;
+            if (tmp <= 0)
+            {
+                break;
+            }
+        }
+        if (num == tmp_result)
+        {
+            result = i;
+            break;
+        }
+    }
+    cout << result << endl;
+    return 0;
+}
