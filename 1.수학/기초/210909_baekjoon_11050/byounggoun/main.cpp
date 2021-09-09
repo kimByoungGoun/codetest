7f5401303e10bcafa5b8a3bbe7e472c3470e897e
kgun610 <kgun610@naver.com>
2021-09-09 11:37:42 +0900

    20210909 baekjoon_11050 이항 계수 1

---
+++ 1.수학/기초/210909_baekjoon_11050/byounggoun/main.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int N, K, result = 1;
+    cin >> N >> K;
+    for (int i = 0; i < K; i++)
+    {
+        result = result * (N - i);
+    }
+    for (int i = 0; i < K; i++)
+    {
+        result = result / (K - i);
+    }
+    cout << result << endl;
+    return 0;
+}
