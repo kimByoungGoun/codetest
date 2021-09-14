22c5d99142c3a4020779ad57e2eba17054a345a4
kgun610 <kgun610@naver.com>
2021-09-14 13:57:15 +0900

    20210914 baekjoon_5086 배수와 약수

---
+++ 1.수학/기초/210914_baekjoon_5086/byounggoun/main.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a, b, result;
+    
+    for (;;)
+    {
+        result = 0;
+        cin >> a >> b;
+        if (a == 0 && b == 0)
+        {
+            break;
+        }
+        if (a > b)
+        {
+            if (a % b == 0)
+            {
+                cout << "multiple" << endl;
+                result = 1;
+            }
+        }
+        else
+        {
+            if (b % a == 0)
+            {
+                cout << "factor" << endl;
+                result = 1;
+            }
+        }
+        if (result == 0)
+        {
+            cout << "neither" << endl;
+        }
+    }
+    return 0;
+}
