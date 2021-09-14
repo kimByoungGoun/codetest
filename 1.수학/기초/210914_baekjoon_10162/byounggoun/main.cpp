ee4b0d53969d19e24b123498ec41224da38d16d9
kgun610 <kgun610@naver.com>
2021-09-14 15:17:34 +0900

    20210914 baekjoon_10162 전자레인지

---
+++ 1.수학/기초/210914_baekjoon_10162/byounggoun/main.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int input, a,b,c;
+    cin >> input;
+    a = input / 300;
+    input = input % 300;
+    
+    b = input / 60;
+    input = input % 60;
+
+    c = input / 10;
+    input = input % 10;
+    if (input != 0)
+    {
+        cout << -1 << endl;
+    }
+    else
+    {
+        cout << a << " " << b << " " << c << endl;
+    }
+    return 0;
+}
