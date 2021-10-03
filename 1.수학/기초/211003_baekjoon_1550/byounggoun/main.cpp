0c808196f1be0789639d5e099276cb251c9bd053
unknown <kgun610@naver.com>
2021-10-03 23:00:29 +0900

    20211003 baekjoon_1550 16진수

---
+++ 1.수학/기초/211003_baekjoon_1550/byounggoun/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#define endl "\n"
+using namespace std;
+
+void init()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+
+
+int main(void)
+{
+	init();
+	string in;
+	cin >> in;
+
+	cout << stoi(in, NULL, 16) << endl;
+	return 0;
+}
+
+/*
+int N,M,K;
+cin >> N >> M >> K;
+int i;
+for (i = 0;;)
+{
+	if (N <= 0 || M <= 0)
+	{
+		break;
+	}
+	else
+	{
+		N = N - 2;
+		M = M - 1;
+		i++;
+	}
+
+	if (K > 0)
+	{
+		i--;
+		K = K - 3;
+	}
+
+}
+cout << i<< endl;*/
