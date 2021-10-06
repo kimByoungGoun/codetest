2467fd98a1815221f90a4a266a79e7b235e8e5c7
kgun610 <kgun610@naver.com>
2021-10-06 11:56:10 +0900

    20211006 baekjoon_4766 일반 화학 실험

---
+++ 1.수학/기초/211006_baekjoon_4766/byounggoun/main.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    cout << fixed;
+    cout.precision(2);
+    double before, after;
+    cin >> before;
+    for (;;)
+    {
+        cin >> after;
+        if (after == 999)
+        {
+            break;
+        }
+        cout << after - before << endl;
+        before = after;
+    }
+    return 0;
+}
